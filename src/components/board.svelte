ffe6e1f7dd363ea2c820b911013d9243d8ff8470
Justin McKinney <justinmckinney.md@gmail.com>
2024-01-27 19:41:37 -0500

    feat: basic minesweeper

---
+++ src/components/board.svelte
@@ -0,0 +1,105 @@
+<script>
+	import Tile from './tile.svelte';
+	const SIZE = 9;
+	let bombsPlaced = false;
+
+	let tiles = createEmptyBoard();
+
+	function createEmptyBoard() {
+		let data = [];
+		for (let i = 0; i < SIZE; i++) {
+			for (let j = 0; j < SIZE; j++) {
+				data.push({ value: 0, visible: false });
+			}
+		}
+		return data;
+	}
+
+	function setupBombs() {
+		let bombCount = 10;
+		while (bombCount > 0) {
+			const placementIndex = Math.round(Math.random() * SIZE * SIZE) - 1;
+			if (tiles[placementIndex].value === '💣') {
+				continue;
+			}
+			tiles[placementIndex].value = '💣';
+			bombCount -= 1;
+		}
+
+		updateBombCounts();
+	}
+
+	function getTileIndex(row, col) {
+		return row * SIZE + col;
+	}
+
+	function isBombAt(row, col) {
+		if (row < 0 || row > SIZE || col < 0 || col > SIZE) {
+			return false;
+		}
+
+		let index = getTileIndex(row, col);
+		if (index < 0 || index >= tiles.length) {
+			return false;
+		}
+		return tiles[index].value === '💣';
+	}
+
+	function updateBombCounts() {
+		for (let row = 0; row < SIZE; row++) {
+			for (let col = 0; col < SIZE; col++) {
+				const tileIndex = getTileIndex(row, col);
+				if (tiles[tileIndex].value === '💣') {
+					continue;
+				}
+				const count = getTileBombCount(row, col);
+				tiles[tileIndex].value = count;
+			}
+		}
+	}
+
+	function getTileBombCount(row, col) {
+		let count = 0;
+		const rowOffsets = [-1, 0, 1];
+		const colOffsets = [-1, 0, 1];
+		for (let deltaRow of rowOffsets) {
+			for (let deltaCol of colOffsets) {
+				if (isBombAt(row + deltaRow, col + deltaCol)) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	/**
+	 *
+	 * @param {number} index
+	 */
+	function tileClicked(index) {
+		if (!bombsPlaced) {
+			setupBombs();
+			bombsPlaced = true;
+		}
+		tiles[index].visible = true;
+		const clickedBomb = tiles[index].value === '💣';
+	}
+
+	function resetBoard() {
+		tiles = createEmptyBoard();
+		bombsPlaced = false;
+	}
+</script>
+
+<div class="flex-col">
+	<div class="grid grid-cols-9 grid-rows-9">
+		{#each tiles as tile, i}
+			<button on:click={() => tileClicked(i)}>
+				<Tile value={tile.value} visible={tile.visible} />
+			</button>
+		{/each}
+	</div>
+	<div class="flex-row">
+		<button on:click={resetBoard} class="p-4 m-4 btn variant-filled">Reset</button>
+	</div>
+</div>
---
+++ src/components/tile.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let value;
+	export let visible;
+
+	$: backgroundColor = visible && value === '💣' ? 'bg-red-900' : visible ? '' : 'bg-slate-400';
+</script>
+
+<div class="border-2 border-white w-16 h-16 {backgroundColor} flex items-center justify-center">
+	{#if visible && value !== 0}
+		<h3 class="h3">
+			{value}
+		</h3>
+	{/if}
+</div>
